b52c3750cbc7c9374d60134ef70728ee866617e5
Kevin Rudissaar <kevin.rudissaar@gmail.com>
2019-10-22 16:50:43 +0100

    Improved kde-neon-install-scripts/install-chatty.sh script.

--- kde-neon-install-scripts/install-chatty.sh
+++ kde-neon-install-scripts/install-chatty.sh
@@ -1,57 +1,60 @@
 #!/usr/bin/env bash
 # Script that installs chatty application and related dependencies.
 
-VERSION='0.9.6'
+VERSION='0.10'
 ARCHIVE_URL="https://github.com/chatty/chatty/releases/download/v${VERSION}/Chatty_${VERSION}.zip"
 ICON_URL='http://img.murda.eu/ch/chatty.png'
 POOL='/usr/local'
 
-SUDO=''
-
+# You need root permissions to run this script.
 if [[ "${UID}" != '0' ]]; then
-    SUDO=$(which sudo)
-
-    if [[ "${?}" != '0' ]]; then
-        echo "> Unable to find 'sudo' from your environment's PATH variable."
-        echo '> Aborting.'
-    fi
+    echo "> Unable to find 'sudo' from your environment's PATH variable."
+    echo '> Aborting.'
+    exit 1
 fi
 
-${SUDO} apt update
+# Update repositories.
+apt update
 
+# Capture commands and install them if they are not already installed.
 WGET=$(which wget)
 
 if [[ "${?}" != '0' ]]; then
-    ${SUDO} apt install wget -y
+    apt install wget -y
 fi
 
 UNZIP=$(which unzip)
 
 if [[ "${?}" != '0' ]]; then
-    ${SUDO} apt install unzip -y
+    apt install unzip -y
 fi
 
 JAVA=$(which java)
 
 if [[ "${?}" != '0' ]]; then
-    ${SUDO} apt install openjdk-11-jre -y
+    apt install openjdk-11-jre -y
 fi
 
+# Create directory for chatty.
 CHATTY_DIR="${POOL}/chatty"
 mkdir -p "${CHATTY_DIR}"
 
+# Download and extract chatty archive.
 ARCHIVE_PATH="chatty-${VERSION}.zip"
-wget "${ARCHIVE_URL}" -O "${ARCHIVE_PATH}"
+"${WGET}" "${ARCHIVE_URL}" -O "${ARCHIVE_PATH}"
 
-unzip "${ARCHIVE_PATH}" -d "${CHATTY_DIR}"
+"${UNZIP}" "${ARCHIVE_PATH}" -d "${CHATTY_DIR}"
 
+# Create directory for chatty icons if it doesn' exist.
 CHATTY_ICON_DIR="${POOL}/icons"
 [[ -d "${CHATTY_ICON_DIR}" ]] || mkdir -p "${CHATTY_ICON_DIR}"
 
+# Download icon for chatty from internet if it doesn't exist locally.
 if [[ ! -f "${CHATTY_ICON_DIR}/chatty.png" ]]; then
-    wget "${ICON_URL}" -O "${CHATTY_ICON_DIR}/chatty.png"
+    "${WGET}" "${ICON_URL}" -O "${CHATTY_ICON_DIR}/chatty.png"
 fi
 
+# Generate desktop entry for chatty application.
 cat > "${POOL}/share/applications/chatty.desktop" <<EOL
 [Desktop Entry]
 Version=${VERSION}
@@ -67,6 +70,7 @@ StartupNotify=true
 Categories=Network;InstantMessaging;
 EOL
 
+# Cleanup.
 rm "${ARCHIVE_PATH}"
 
 echo '> Finished.'
