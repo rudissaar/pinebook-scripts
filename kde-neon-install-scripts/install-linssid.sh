78e290761b0d44819bbe9513d9a3cbc3e751d021
Kevin Rudissaar <kevin.rudissaar@gmail.com>
2020-01-11 19:36:34 +0000

    Improved kde-neon-install-scripts/install-linssid.sh script.

--- kde-neon-install-scripts/install-linssid.sh
+++ kde-neon-install-scripts/install-linssid.sh
@@ -8,19 +8,45 @@ if [[ "${UID}" != '0' ]]; then
     exit 1
 fi
 
-SED=$(which sed)
+# Function that checks if required binary exists and installs it if necessary.
+ENSURE_PACKAGE () {
+    REQUIRED_BINARY=$(basename "${1}")
+    REPO_PACKAGES="${*:2}"
 
-if [[ "${?}" != '0' ]]; then
-    apt install sed -y
-fi
+    if [[ "${REQUIRED_BINARY}" != '-' ]]; then
+        [[ -n "${REPO_PACKAGES}" ]] || REPO_PACKAGES="${REQUIRED_BINARY}"
 
-# Install linssid package.
-apt install linssid -y --reinstall
+        if command -v "${REQUIRED_BINARY}" 1> /dev/null; then
+            REPO_PACKAGES=''
+        fi  
+    fi  
+
+    [[ -n "${REPO_PACKAGES}" ]] || return
+
+    if [[ "${REPO_REFRESHED}" == '0' ]]; then
+        echo '> Refreshing package repository.'
+        apt-get update 1> /dev/null
+        REPO_REFRESHED=1
+    fi  
+
+    for REPO_PACKAGE in ${REPO_PACKAGES}
+    do  
+        apt-get install -y "${REPO_PACKAGE}"
+    done
+}
+
+# Variable that keeps track if repository is already refreshed.
+REPO_REFRESHED=0
+
+# Install packages if necessary.
+ENSURE_PACKAGE 'sed'
+ENSURE_PACKAGE 'linssid'
 
 if [[ -f '/usr/share/applications/linssid.desktop' ]]; then
     ${SED} -i 's/^Exec=linssid$/Exec=pkexec --user root env DISPLAY=$DISPLAY XAUTHORITY=$XAUTHORITY linssid/g' \
         '/usr/share/applications/linssid.desktop'
 fi
 
+# Let user know that script has finished its job.
 echo '> Finished.'
 
