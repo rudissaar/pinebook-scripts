bfee886344c88bb3223e3006347c6a4313f21601
Kevin Rudissaar <kevin.rudissaar@gmail.com>
2020-01-14 22:18:16 +0000

    Improved kde-neon-config-scripts/firefox-discord-app.sh script.

--- kde-neon-config-scripts/firefox-discord-app.sh
+++ kde-neon-config-scripts/firefox-discord-app.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
+# Script that sets up Dicord shortcut that will appear in applications menu.
 
+ICON_URL='https://raw.githubusercontent.com/rudissaar/img-murda-eu/master/di/discord.png'
 PACKAGE_POOL='/usr/local'
 
 # You need root permissions to run this script.
@@ -41,6 +43,18 @@ REPO_REFRESHED=0
 
 # Install packages if necessary.
 ENSURE_PACKAGE 'firefox'
+ENSURE_PACKAGE 'wget'
+
+# Create directory for icons if it doesn't already exist.
+ICONS_PATH="${PACKAGE_POOL}/share/icons"
+[[ -d "${ICONS_PATH}" ]] || mkdir -p "${ICONS_PATH}"
+ICON_PATH="${ICONS_PATH}/discord.png"
+
+# Download icon for discord shortcut.
+if ! wget "${ICON_URL}" -O "${ICON_PATH}"; then
+    echo '> Unable to download icon for shortcut.'
+    echo '> Skipping.'
+fi
 
 # Generate desktop entry for discord shortcut.
 [[ -d "${PACKAGE_POOL}/share/applications" ]] || mkdir -p "${PACKAGE_POOL}/share/applications"
@@ -48,16 +62,14 @@ ENSURE_PACKAGE 'firefox'
 cat > "${PACKAGE_POOL}/share/applications/discord.desktop" <<EOL
 [Desktop Entry]
 Name=Discord
-GenericName=Discord
-Comment=Discord
+GenericName=Internet Messenger
+Comment=All-in-one voice and text chat for gamers that's free, secure, and works on both your desktop and phone.
 Exec=firefox --new-tab https://discordapp.com/activity
-Terminal=false
-X-MultipleArgs=false
 Type=Application
-Icon=discord
-StartupNotify=true
+Icon=${ICON_PATH}
 Categories=Network;InstantMessaging;
 EOL
 
 # Let user know that script has finished its job.
 echo '> Finished.'
+
