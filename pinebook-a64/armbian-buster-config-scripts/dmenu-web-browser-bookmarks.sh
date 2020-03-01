43f5e3fd5cfb7f2322f625dba4ffb53a700dac6b
Kevin Rudissaar <kevin.rudissaar@gmail.com>
2020-03-01 16:28:34 +0000

    Improved pinebook-a64/armbian-buster-config-scripts/dmenu-web-browser-bookmarks.sh

--- pinebook-a64/armbian-buster-config-scripts/dmenu-web-browser-bookmarks.sh
+++ pinebook-a64/armbian-buster-config-scripts/dmenu-web-browser-bookmarks.sh
@@ -47,33 +47,37 @@ ENSURE_PACKAGE 'grep'
 
 # Create executable script.
 [[ -d "${PACKAGE_POOL}/bin" ]] || mkdir -p "${PACKAGE_POOL}/bin"
-BOOKMARKS_SCRIPT_PATH="${PACKAGE_POOL}/bin/bookmarks.sh"
+BOOKMARKS_SCRIPT_PATH="${PACKAGE_POOL}/bin/open-bookmark.sh"
 
 cat > "${BOOKMARKS_SCRIPT_PATH}" <<EOL
 #!/usr/bin/env bash
+# Script that enables you to open web browser bookmarks from dmenu.
 
-HANDLE_DEFAULT () {
-    if [[ ! -z "\${1}" ]]; then
-        "\${1}" "\${URL}"
-    elif [[ ! -z "\${BROWSER}" ]]; then
-        "\${BROWSER}" "\${URL}"
-    fi
-}
+COMMAND=''
+COMMAND_FALLBACK='firefox'
 
-BOOKMARKS="${PACKAGE_POOL}/share/bookmarks/bookmarks"
-USER_BOOKMARKS="\${HOME}/.config/bookmarks"
+# Attempt to read in selected web browser from environment variable.
+[[ ! -z "\${BROWSER}" ]] && COMMAND="\${BROWSER}"
 
-if [[ -f "\${USER_BOOKMARKS}" ]]; then
-    BOOKMARKS="\${USER_BOOKMARKS}"
-fi
+# At this point, if script was unable to detect web browser we will fallback instead.
+[[ -z "\${COMMAND}" ]] && COMMAND="\${COMMAND_FALLBACK}"
 
+BOOKMARKS="${PACKAGE_POOL}/share/bookmarks/bookmarks.txt"
+USER_BOOKMARKS="\${HOME}/.local/share/bookmarks/bookmarks.txt"
+[[ -f "\${USER_BOOKMARKS}" ]] && BOOKMARKS="\${USER_BOOKMARKS}"
+
+# If bookmarks file doesn't exist then let's just exit.
+[[ -f "\${BOOKMARKS}" ]] || exit 1
+
+# Display list of available bookmarks to open.
 URL=\$(sort "\${BOOKMARKS}" | sed 's/:.*//' | dmenu -i -p 'Select a Bookmark' | xargs -I % grep "%:" "\${BOOKMARKS}" | sed 's/.*://')
 
-if [[ "\${URL}" ]]; then
-    case "\${1}" in
-        "firefox") firefox --new-tab "\${URL}";;
-        "firefox-esr") firefox-esr --new-tab "\${URL}";;
-        *) HANDLE_DEFAULT;;
+# If for some reason selected filename evaluates to empty string, we cancel launching web browser.
+if [[ ! -z "\${URL}" ]]; then
+    case "\${COMMAND}" in
+        "firefox") "\${COMMAND}" --new-tab "\${URL}" 2> /dev/null;;
+        "firefox-esr") "\${COMMAND}" --new-tab "\${URL}" 2> /dev/null;;
+        *) "\${COMMAND}" "\${URL}";;
     esac
 fi
 
@@ -84,13 +88,13 @@ chmod +x "${BOOKMARKS_SCRIPT_PATH}"
 
 # Create file for global bookmarks.
 [[ -d "${PACKAGE_POOL}/share/bookmarks" ]] || mkdir -p "${PACKAGE_POOL}/share/bookmarks"
-touch "${PACKAGE_POOL}/share/bookmarks/bookmarks"
+touch "${PACKAGE_POOL}/share/bookmarks/bookmarks.txt"
 
 # Create file that can be used/sourced for uninstalling.
 cat > "${PACKAGE_POOL}/share/bookmarks/uninstall.txt" <<EOL
-rm "${PACKAGE_POOL}/share/bookmarks/bookmarks"
+rm "${PACKAGE_POOL}/share/bookmarks/bookmarks.txt"
 rm "${PACKAGE_POOL}/share/bookmarks/uninstall.txt"
-rm "${PACKAGE_POOL}/bin/bookmarks.sh"
+rm "${PACKAGE_POOL}/bin/open-bookmark.sh"
 rmdir "${PACKAGE_POOL}/share/bookmarks" 2> /dev/null
 EOL
 
