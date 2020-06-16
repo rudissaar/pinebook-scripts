d79ecd632b362ba46b459ac2e8ab5ae7376d2ce5
Kevin Rudissaar <kevin.rudissaar@gmail.com>
2020-06-16 13:29:19 +0000

    Increased NetBeans version in pinebook-pro/debian-desktop-install-scripts/install-apache-netbeans.sh script.

--- pinebook-pro/debian-desktop-install-scripts/install-apache-netbeans.sh
+++ pinebook-pro/debian-desktop-install-scripts/install-apache-netbeans.sh
@@ -2,7 +2,7 @@
 # Script that installs Apache NetBeans IDE on your system.
 
 PACKAGE_POOL="/usr/local"
-VERSION='11.3'
+VERSION='12.0'
 USE_ICON_FROM_ARCHIVE=1
 
 if [[ "${VERSION}" == '11.0' ]]; then
